729932886118f71944a2a08d27fbe5e8b4015ee9
Rostyslav Bryzgunov <kottenator@gmail.com>
2018-04-01 22:26:00 -0400

    Page header - add logout link

--- src/project/auth/urls.py
+++ src/project/auth/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import AccessPage
@@ -6,4 +7,5 @@ app_name = 'project_auth'
 
 urlpatterns = [
     path('access/', AccessPage.as_view(), name='access'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
